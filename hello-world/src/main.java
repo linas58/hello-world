8fdf255427de92d7702cc090b0fe7e303bcce2f0
Linas Alinskas <Linas58@yahoo.com>
2015-09-21 19:40:50 -0400

    second commit

--- hello-world/src/main.java
+++ hello-world/src/main.java
@@ -2,7 +2,8 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
+		// Lets make a change
 		System.out.print("hello world!");
 	}
 
